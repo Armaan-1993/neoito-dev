f78ba06513fd30c0f0a4cb59a4491593e6f90648
armaan <armaanmathew88@gmail.com>
2020-10-20 01:40:33 +0530

    carousel

--- Workshop2/app.js
+++ Workshop2/app.js
@@ -36,56 +36,35 @@ function myFunction() {
 // }
 
 
-
-
-
-
-
 // Carousel
-// $(document).ready(function() {
-//   $("#myCarousel").on("slide.bs.carousel", function(e) {
-//     var $e = $(e.relatedTarget);
-//     var idx = $e.index();
-//     var itemsPerSlide = 3;
-//     var totalItems = $(".carousel-item").length;
 
-//     if (idx >= totalItems - (itemsPerSlide - 1)) {
-//       var it = itemsPerSlide - (totalItems - idx);
-//       for (var i = 0; i < it; i++) {
-//         // append slides to end
-//         if (e.direction == "left") {
-//           $(".carousel-item")
-//             .eq(i)
-//             .appendTo(".carousel-inner");
-//         } else {
-//           $(".carousel-item")
-//             .eq(0)
-//             .appendTo($(this).find(".carousel-inner"));
-//         }
-//       }
-//     }
-//   });
-// });
-$('#recipeCarousel').carousel({
-  interval: 10000
-})
+$(document).ready(function() {
+  $("#myCarousel").on("slide.bs.carousel", function(e) {
+    var $e = $(e.relatedTarget);
+    var idx = $e.index();
+    var itemsPerSlide = 3;
+    var totalItems = $(".carousel-item").length;
 
-$('.carousel .carousel-item').each(function(){
-    var minPerSlide = 3;
-    var next = $(this).next();
-    if (!next.length) {
-    next = $(this).siblings(':first');
-    }
-    next.children(':first-child').clone().appendTo($(this));
-    
-    for (var i=0;i<minPerSlide;i++) {
-        next=next.next();
-        if (!next.length) {
-        	next = $(this).siblings(':first');
-      	}
-        
-        next.children(':first-child').clone().appendTo($(this));
+    if (idx >= totalItems - (itemsPerSlide - 1)) {
+      var it = itemsPerSlide - (totalItems - idx);
+      for (var i = 0; i < it; i++) {
+        // append slides to end
+        if (e.direction == "left") {
+          $(".carousel-item")
+            .eq(i)
+            .appendTo(".carousel-inner");
+        } else {
+          $(".carousel-item")
+            .eq(0)
+            .appendTo($(this).find(".carousel-inner"));
+        }
       }
+    }
+  });
 });
+
+
+
+
 
 
