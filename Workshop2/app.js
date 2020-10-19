c6a88b98ea4cbf7e91090116791af19e8294be31
armaan <armaanmathew88@gmail.com>
2020-10-19 17:54:16 +0530

    hamburger menu completed

--- Workshop2/app.js
+++ Workshop2/app.js
@@ -1,14 +1,17 @@
 // Hamburger Menu
 
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function myFunction() {
   var x = document.getElementById("myLinks");
+  
   if (x.style.display === "block") {
     x.style.display = "none";
   } else {
     x.style.display = "block";
   }
 } 
+
+// let x = document.getElementById("myLinks");
+
 
 
 
